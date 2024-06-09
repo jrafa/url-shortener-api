a758d9dc3aadee8ffaf6d24592698ac68f85566d
jrafa <rafalska.justyna@gmail.com>
2024-06-09 23:56:20 +0200

    add ruff changes and genrator short code

---
+++ api/constants.py
@@ -0,0 +1,4 @@
+import string
+
+SHORT_CODE_CHARS: str = string.ascii_letters + string.digits
+SHORT_CODE_MAX_LENGTH: int = 7
---
+++ api/helpers.py
@@ -0,0 +1,7 @@
+import random
+
+from api.constants import SHORT_CODE_MAX_LENGTH, SHORT_CODE_CHARS
+
+
+def generate_short_code(length: int = SHORT_CODE_MAX_LENGTH) -> str:
+    return ''.join(random.choices(SHORT_CODE_CHARS, k=length))
