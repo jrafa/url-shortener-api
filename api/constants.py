c8eec8a0eeefe3c570e5f19a9dc2346f39538ae7
jrafa <rafalska.justyna@gmail.com>
2024-06-10 00:17:47 +0200

    add serializer

--- api/constants.py
+++ api/constants.py
@@ -3,3 +3,4 @@ import string
 SHORT_CODE_CHARS: str = string.ascii_letters + string.digits
 SHORT_CODE_MAX_LENGTH: int = 7
 MAX_LENGTH_ORIGINAL_URL: int = 200
+MAX_LENGTH_SHORTENED_URL: int = 50
--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,12 @@
 from django.core.validators import URLValidator
 from rest_framework import serializers
 
-from api.constants import MAX_LENGTH_ORIGINAL_URL
+from api.constants import MAX_LENGTH_ORIGINAL_URL, MAX_LENGTH_SHORTENED_URL
 
 
 class OriginalUrlSerializer(serializers.Serializer):
     original_url = serializers.CharField(max_length=MAX_LENGTH_ORIGINAL_URL, validators=[URLValidator()])
+
+
+class ShortenedUrlSerializer(serializers.Serializer):
+    shortened_url = serializers.CharField(max_length=MAX_LENGTH_SHORTENED_URL, validators=[URLValidator()])
